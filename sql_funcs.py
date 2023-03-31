4e5bfaf563823ca07883027fbc5940651358be84
robbyweeds <96435093+robbyweeds@users.noreply.github.com>
2023-03-31 12:37:22 -0400

    Update sql_funcs.py
    extra functions for pyinstaller

--- sql_funcs.py
+++ sql_funcs.py
@@ -16,61 +16,121 @@ from openpyxl import Workbook
 
 # **************CREATE TABLE COMMAND**************
 def createTable():
-    conn = dbconnect = sqlite3.connect('newdb1', detect_types=sqlite3.PARSE_DECLTYPES)
-    c = conn.cursor()
+  conn = sqlite3.connect('newdb1', detect_types=sqlite3.PARSE_DECLTYPES)
+  c = conn.cursor()
+  list_of_tables = c.execute(
+    """SELECT * FROM sqlite_master WHERE type='table'; """).fetchall()
+  if list_of_tables == []:
     table = '''
         CREATE TABLE books (
             title VARCHAR(100) NOT NULL,
             first_name VARCHAR(50),
             last_name VARCHAR(50),
-            date DATE,
+            input_date VARCHAR(40),
             cat VARCHAR(30),
             year INTEGER
         )'''
-    c.execute("DROP TABLE IF EXISTS books;")
+    # c.execute("DROP TABLE IF EXISTS books;")
     c.execute(table)
+    conn.close()
+  else:
     conn.close()
 
 
 
 # **************ALTER TABLE COMMAND**************
 def alterTable():
-    conn = dbconnect = sqlite3.connect('newdb1', detect_types=sqlite3.PARSE_DECLTYPES)
-    c = conn.cursor()
-    c.execute("ALTER TABLE books ADD COLUMN cat VARCHAR(50);")
-    conn.commit()
-    conn.close()
+  conn = sqlite3.connect('newdb1', detect_types=sqlite3.PARSE_DECLTYPES)
+  c = conn.cursor()
+  c.execute("ALTER TABLE books ADD COLUMN cat VARCHAR(50);")
+  conn.commit()
+  conn.close()
 
 
 # **************HARD CODE IN A ROW**************
 def insertRow(a, b, c, d, e):
-    conn = dbconnect = sqlite3.connect('newdb1', detect_types=sqlite3.PARSE_DECLTYPES)
-    c = conn.cursor()
-    c.execute( "INSERT INTO books (title, first_name, last_name, year, date) VALUES (?, ?, ?, ?, ?);",(a, b, c, d, e))
-    conn.commit()
-    conn.close()
+  conn = sqlite3.connect('newdb1', detect_types=sqlite3.PARSE_DECLTYPES)
+  c = conn.cursor()
+  c.execute( "INSERT INTO books (title, first_name, last_name, year, input_date) VALUES (?, ?, ?, ?, ?);",(a, b, c, d, e))
+  conn.commit()
+  conn.close()
 
 # **************HARD CODE DELETE ALL ROWS**************
 def deleteRows():
-    conn = dbconnect = sqlite3.connect('newdb1', detect_types=sqlite3.PARSE_DECLTYPES)
-    c = conn.cursor()
-    c.execute("DELETE FROM books;")
-    conn.commit()
-    conn.close()
+  conn =  sqlite3.connect('newdb1', detect_types=sqlite3.PARSE_DECLTYPES)
+  c = conn.cursor()
+  c.execute("DELETE FROM books;")
+  conn.commit()
+  conn.close()
+
+# *************DELETE SPECIFIC ROW**********************
+def deleteRow(row):
+  conn =  sqlite3.connect('newdb1', detect_types=sqlite3.PARSE_DECLTYPES)
+  c = conn.cursor()
+  c.execute("DELETE FROM books WHERE title = ?;", (row,))
+  conn.commit()
+  conn.close()
 
 # **************CONNECTION COMMIT**************
 # conn.commit()
 
 # **************RETRIVE BOOK ROWS**************
 def selectRows():
-    conn = dbconnect = sqlite3.connect('newdb1', detect_types=sqlite3.PARSE_DECLTYPES)
-    c = conn.cursor()
-    backs = c.execute('''
-        SELECT * FROM books
-        ''')
-    for back in backs:
-        print(back)
-    conn.close()
+  conn = sqlite3.connect('newdb1', detect_types=sqlite3.PARSE_DECLTYPES)
+  c = conn.cursor()
+  backs = c.execute('''
+      SELECT * FROM books
+      ''')
+  for back in backs:
+      print(back)
+  conn.close()
 
 # **************ORIGINAL CONNECTION CLOSE**************
 # conn.close()
+
+
+
+# **************PRINT ROWS TO CONSOLE**************
+def print_rows():
+  conn = sqlite3.connect('newdb1', detect_types=sqlite3.PARSE_DECLTYPES)
+  c = conn.cursor()
+  backs = c.execute('''
+    SELECT rowid, title, first_name, last_name, cat, year, input_date FROM books
+    ''')
+  for back in backs:
+      print(back)
+
+  conn.close()
+
+# **************GRAB AND PRINT ROWS FROM PANDAS**************
+def panda_rows():
+  conn = sqlite3.connect('newdb1', detect_types=sqlite3.PARSE_DECLTYPES)
+  surveys_df = pd.read_sql_query("SELECT * from books", conn)
+  print(surveys_df)
+
+  conn.close()
+
+
+# **************RETRIVE Specific BOOK ROWS**************
+def editSingleRow(book, fname, lname, year):
+  # conn = sqlite3.connect('newdb1', detect_types=sqlite3.PARSE_DECLTYPES)
+  # c = conn.cursor()
+  if fname == '' and lname == '':
+    conn = sqlite3.connect('newdb1', detect_types=sqlite3.PARSE_DECLTYPES)
+    c = conn.cursor()
+    print('yearssss')
+    reply = c.execute('UPDATE books SET year = ? WHERE title = ?', (year, book))
+    conn.close()
+  elif fname == '' and year == '': 
+    reply = c.execute('UPDATE books SET last_name = ? WHERE title = ?', (lname, book))
+  elif year == 0 and lname == '':
+    reply = c.execute('UPDATE books SET first_name = ? WHERE title = ?', (fname, book))
+  elif fname == '': 
+    reply = c.execute('UPDATE books SET year = ?, last_name= ? WHERE title = ?', (year, lname, book))
+  elif year == 0:
+    reply = c.execute('UPDATE books SET first_name = ?, last_name = ?) WHERE title = ?', (fname, lname, book))
+  elif lname == '':
+    reply = c.execute('UPDATE books SET first_name = ?, year = ? WHERE title = ?', (fname, year, book)) 
+  else:
+    reply = c.execute('UPDATE books SET last_name= ?, first_name = ?, year = ? WHERE title = ?', (lname, fname, year, book)) 
+  # conn.close()
